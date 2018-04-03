729c8362becf0dee545ce1c1b55a1f0fc41751cc
Ahmed Nitul <ahmednitul@gmail.com>
2018-04-03 18:47:48 +0600

    Create clean.php

---
+++ clean.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <title>@N - MA</title>
+
+  <style>
+    * {
+      margin: 0; padding: 0;
+    }
+    html, body {
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+    }
+    body {
+      text-align: center;
+      font-family: 'proxima-nova', Helvetica;
+    }
+    #n {
+      position: absolute;
+      bottom: 0; left: 0; right: 0;
+      margin-bottom: 69px;
+    }
+    #n a {
+      color: rgba(255,255,255,0.5);
+      text-decoration: none;
+      margin: 10px;
+      font-size: 12px;
+    }
+    #n a:first-child {
+      position: absolute;
+      bottom: 40px; left: 0; right: -480px;
+    }
+    #n a:last-child {
+      float: right;
+    }
+  </style>
+
+<script type="text/javascript">
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+</script>
+
+</head>
+<body onload="toggle_visibility('n');">
+<iframe src="https://mirrorace.com/m/embed/<?php echo $_GET["!"]; ?>" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;" scrolling="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"><meta http-equiv="refresh" content="0;url=https://mirrorace.com/m/embed/<?php echo $_GET["!"]; ?>" /></iframe>
+
+  <div id="n">
+    <a href="#" onclick="toggle_visibility('n');"><img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png" height="25" width="25"><a/>
+    <a href="http://spklmis.com/?p=28&s=60689&pp=1&v=0" target="_blank" rel="follow"> <img src="//misslk.com/s.php?w=468&h=60&m=0&i=0&l=US&n=2" alt="hot girl striptease sexy nude poledance desktop stripper" height="60" width="468"> </a>
+    <iframe width="0" height="0" src="http://spklmis.com/?p=28&s=60689&pp=1&v=0" frameborder="0"></iframe>
+  </div>
+</body>
+</html>
